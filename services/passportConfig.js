51ccfc643e46601bd0600cda9856cb8cd4babe74
p-b-3 <patrrb3@gmail.com>
2020-02-10 19:37:21 -0500

    1

--- services/passportConfig.js
+++ services/passportConfig.js
@@ -3,7 +3,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
-// User is model class, can use to create new model instance
+// User is model class, can use to create a new model instance
 const User = mongoose.model("users");
 
 // add cookie to user
