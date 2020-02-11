1cc22497b4da6dc1147188414825dc0d793bd7a8
p-b-3 <patrrb3@gmail.com>
2020-02-10 22:01:20 -0500

    auth1

--- services/passportConfig.js
+++ services/passportConfig.js
@@ -26,7 +26,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
+      callbackURL: "/auth/google/callback",
+      proxy: true
     },
     // callback function with user profile and accessToken from google
     (accessToken, refreshToken, profile, done) => {
