6caca9fc4247c22ec93b1650d75900102e5137bc
p-b-3 <patrrb3@gmail.com>
2020-02-17 15:53:55 -0500

    user logic

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -3,7 +3,7 @@ const React = import("react");
 const Dashboard = () => {
   return (
     <div className="fixed-action-btn">
-      <a className="btn-flaoting btn-large red">
+      <a className="btn-flaoting btn-large deep-purple accent-1">
         <i className="material-icons">add</i>
       </a>
     </div>
