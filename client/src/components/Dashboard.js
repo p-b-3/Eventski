5c4508e2aa38b88bd96375e158dc3038dbd0ab1d
p-b-3 <patrrb3@gmail.com>
2020-02-17 16:05:06 -0500

    dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div className="fixed-action-btn">
-      <a className="btn-flaoting btn-large deep-purple accent-1">
+      <Link
+        to="/surveys/new"
+        className="btn-flaoting btn-large deep-purple accent-1"
+      >
         <i className="material-icons">add</i>
-      </a>
+      </Link>
     </div>
   );
 };
