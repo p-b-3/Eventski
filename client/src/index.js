e80dc43ce3056662b27de5795d274fa7aab0c84d
p-b-3 <patrrb3@gmail.com>
2020-02-17 13:16:04 -0500

    mailer

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,10 @@ import ReactDom from "react-dom"; //takes root component and where we are lookin
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
-
 import App from "./components/App.js";
 import reducers from "./reducers/index.js";
+import axios from "axios";
+window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk)); //pass in reducers, initial state of app
 
--- services/Mailer.js
+++ services/Mailer.js
@@ -1,5 +1,5 @@
 const sendgrid = require("sendgrid");
-const helper = sendrgrid.mail;
+const helper = sendgrid.mail;
 const keys = require("../config/keys");
 
 class Mailer extends helper.Mail {
