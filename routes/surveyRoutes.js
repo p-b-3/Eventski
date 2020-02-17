c6a672daffce1d7b38e6ef739f30335e160fc25f
p-b-3 <patrrb3@gmail.com>
2020-02-17 13:07:53 -0500

    mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,8 +1,9 @@
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const mongoose = require("mongoose");
-
 const Survey = mongoose.model("surveys");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/SurveyTemplate");
 
 module.exports = async app => {
   app.post("/api/survey", requireLogin, requireCredits, (req, res) => {
@@ -19,9 +20,9 @@ module.exports = async app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
-
     //attempt to create and send email
-
+    const mailer = new Mailer(survey, surveyTemplate(survey)); //second arg is html content
+    mailer.send();
     //email sent succesfully so save survey
   });
 };
