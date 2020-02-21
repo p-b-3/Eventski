6acbd4516f2ba502aae00a0f308fe7597380f65c
p-b-3 <patrrb3@gmail.com>
2020-02-21 12:06:23 -0500

    forms update

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import formFields from "./formFields";
 
-const SurveyFormReview = props => {
+const SurveyFormReview = (props, state) => {
   const reviewFields = formFields.map(field => {
     return (
       <div key={field.name}>
