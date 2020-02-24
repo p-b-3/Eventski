4e3c3a35825ec5a1322179c84f6d74db80aa3f5f
p-b-3 <patrrb3@gmail.com>
2020-02-23 20:39:24 -0500

    webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
 const Mailer = require("../services/Mailer");
-const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
+const surveyTemplate = require("../services/emailTemplates/SurveyTemplate");
 
 const Survey = mongoose.model("surveys");
 
@@ -66,7 +66,6 @@ module.exports = app => {
       dateSent: Date.now()
     });
 
-    // Great place to send an email!
     const mailer = new Mailer(survey, surveyTemplate(survey));
 
     try {
